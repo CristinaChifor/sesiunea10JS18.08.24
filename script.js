593f8d893e0c0a943843b462bc36c0cdd530205c
Cristina.Chifor <cristinachifor93.cc@gmail.com>
2024-08-19 00:23:21 +0300

    Examples of JavaScript Data

--- script.js
+++ script.js
@@ -56,6 +56,58 @@ let numberF = 4;
 let moduloResult = numberE % numberF;
 console.log(`Restul impartirii lui ${numberE} la ${numberF} este egal cu ${moduloResult}.`);
 
+/**
+ Tipuri de date in JavaScript
+ 1. Number
+ */
+
+ // Cum aflam cel mai mare numar de tip NUMBER?
+
+ console.log(Number.MAX_SAFE_INTEGER);
+
+ // Cum aflam cel mai mic numar de tip NUMBER?
+
+ console.log(Number.MIN_SAFE_INTEGER);
+
+ // 2. BigInt
+
+ let bigIntNumber = 123456799833329n;
+ console.log(`BigInt value is ${bigIntNumber}`);
+
+ // 3. String
+
+const myName = "Cristina Chifor";
+
+// Primul caracter dintr-un string
+
+const firstCharInName = myName[0];
+console.log(`Primul caracter din sirul  '${myName}' este ${firstCharInName}.`);
+
+const secondCharInName = myName[1];
+console.log(`Al doilea caracter din sirul  '${myName}' este ${secondCharInName}.`);
+
+// Lungimea unui String
+
+const myNameLength = myName.length;
+console.log(`Lungimea sirului '${myName}' este ${myNameLength}.`);
+
+let lastCharIndex = myNameLength - 1;
+let lastChar = myName[lastCharIndex];
+console.log(`Ultimul caracter din sir este ${lastChar}`);
+
+// Concatenarea unui String
+
+let firstName = "Cristina";
+let lastName = "Chifor";
+let fullName = firstName + " "+ lastName;
+console.log(`Numele meu complet este ${fullName}`);
+
+let upperCase = fullName.toUpperCase();
+console.log(upperCase);
+
+
+
+
 
 
 
