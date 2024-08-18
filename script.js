2ea36ef4cc31dde56823c273e949b9c543e72920
Cristina.Chifor <cristinachifor93.cc@gmail.com>
2024-08-18 22:30:41 +0300

    Examples with arithmetic operators

--- script.js
+++ script.js
@@ -8,4 +8,67 @@ console.log(years);
 age = 36;
 console.log(age);
 
+let username = "Cristina";
+console.log("username: ");
+console.log(username);
+
+const piNumber = 3.14;
+console.log(piNumber);
+// piNumber = 4;
+// console.log(piNumber);
+
+/** Date numerice- Number
+Operatori date numerice*/
+
+/** Operatori aritmetici
+ * Addition +*/ 
+
+let firstNumber = 10;
+let secondNumber = 21;
+let sum = firstNumber + secondNumber;
+console.log(sum);
+console.log(`Rezultatul sumei dintre ${firstNumber} si ${secondNumber} este ${sum}.`);
+
+// Substraction -
+
+let pastAge = age -years;
+console.log(`Acum ${years} aveam varsta de ${pastAge}.`);
+console.log(`Acum ${years} aveam varsta de ${age - years}.`);
+
+// Multiplication *
+
+let numberA = 5;
+let numberB = 10;
+let multiplyResult = numberA * numberB;
+console.log(`${numberA} inmultit cu ${numberB} este egal cu ${multiplyResult}.`)
+
+// Division /
+
+let numberC = 25;
+let numberD = 10;
+let divisionResult = numberC / numberD;
+console.log(`${numberC} impartit la ${numberD} este egal cu ${divisionResult}.`)
+
+// Modulo %
+
+let numberE = 15;
+let numberF = 4;
+let moduloResult = numberE % numberF;
+console.log(`Restul impartirii lui ${numberE} la ${numberF} este egal cu ${moduloResult}.`);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
